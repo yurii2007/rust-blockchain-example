2d19b3dbb62b09be9ef1fde8181253b64401ce85
yurii2007 <yurash862@gmail.com>
2026-03-06 20:45:41 +0200

    app init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  async function onclick() {
+    const res = await fetch('http://localhost:8080/auth/google/login', {
+      method: 'GET',
+      headers: {
+        'Access-Control-Allow-Origin': 'http://localhost:8080'
+      }
+    });
+
+    console.log('res', res);
+  }
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<button {onclick}> login with google </button>
