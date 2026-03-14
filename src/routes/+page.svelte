60d5339e986d219f21286338fe72eeed47d314a8
yurii2007 <yurash862@gmail.com>
2026-03-14 19:58:26 +0200

    palette configuration, shadcn-svelte install

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+  import { Button } from '$lib/components/ui/button';
+
   async function onclick() {
     const res = await fetch('http://localhost:8080/auth/google/login', {
       method: 'GET',
@@ -24,5 +26,14 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
 
-<button {onclick} class="rounded-sm bg-primary-500 p-2">login with google </button>
+<button {onclick} class="bg-primary-500 rounded-sm p-2">login with google </button>
 <button onclick={check_auth} class="mt-2">auth request</button>
+
+<div class="flex flex-wrap items-center justify-start gap-4">
+  <Button variant="default" onclick={console.log}>primary default</Button>
+  <Button variant="secondary" onclick={console.log}>secondary default</Button>
+  <Button variant="destructive" onclick={console.log}>destructive default</Button>
+  <Button variant="outline" onclick={console.log}>outline default</Button>
+  <Button variant="link" onclick={console.log}>link default</Button>
+  <Button variant="ghost" onclick={console.log}>ghost default</Button>
+</div>
