4ac7ad6e3d3789a66a5db3bb660a65708b324775
yurii2007 <yurash862@gmail.com>
2026-03-26 21:17:41 +0200

    setup: new colors, login page mvp

--- src/routes/auth/+page.svelte
+++ src/routes/auth/+page.svelte
@@ -2,20 +2,22 @@
   import { Button } from '$lib/components/ui/button';
 </script>
 
-<div class="flex h-screen w-screen flex-col items-center justify-center gap-10">
-  <section>
-    <span>logo</span>
-    <h1 class="typography-h2 text-center">NeoSpend</h1>
+<main
+  class="mt-auto flex h-screen w-screen flex-col items-center justify-center gap-12 bg-background"
+>
+  <section class="flex flex-col">
+    <span class="typography-h4 mx-auto inline-block text-primary">payments</span>
+    <h1 class="typography-h2 mt-6 text-center">NeoSpend</h1>
     <p class="typography-body1 mt-2 text-center text-muted-foreground">
       Next-gen expense management
     </p>
-    <form
-      class="mt-10 max-w-98 rounded-2xl border border-primary/10 bg-background/80 p-8 backdrop-blur-md"
-    >
-      <p class="typography-h4 text-left">Welcome back</p>
-      <p class="typography-subtitle">Please choose a service to continue</p>
+    <form class="mt-12 max-w-100">
+      <p class="typography-h4 text-center">Welcome back</p>
+      <p class="typography-subtitle mt-1 text-center text-foreground">
+        Select a provider to continue
+      </p>
 
-      <ul class="mt-8 flex w-full flex-col gap-4">
+      <ul class="mt-8 flex w-full flex-col gap-3">
         <li class="w-full">
           <Button variant="outline" class="w-full">google</Button>
         </li>
@@ -24,27 +26,23 @@
         </li>
       </ul>
 
-      <div class="mt-8 flex w-full items-center gap-2">
-        <span class="h-px w-full bg-background"></span>
-        <span class="typography-subtitle whitespace-nowrap uppercase">Secure Access</span>
-        <span class="h-px w-full bg-background"></span>
-      </div>
+      <p
+        class="typography-subtitle2 mt-8 w-full border-t pt-8 text-center whitespace-nowrap uppercase"
+      >
+        Secure Access Provided by NeoSpend
+      </p>
 
-      <p class="typography-subtitle mt-8 text-center">
-        By accessing NeoSpend, you agree to our <a href="/" class="text-primary">Terms</a> and
-        <a href="/" class="text-primary">Privacy Policy</a>.
+      <p class="typography-subtitle2 mt-8 text-center text-foreground">
+        By signing in, you agree to our <a href="/" class="text-muted-foreground">Terms</a> and
+        <a href="/" class="text-muted-foreground">Privacy Policy</a>.
       </p>
     </form>
   </section>
 
-  <footer class="mx-auto mt-10">
-    <ul class="flex flex-row items-center justify-center">
-      <li class="typography-subtitle border-r border-r-accent px-6 last:border-r-transparent">
-        <a href="/">Support</a>
-      </li>
-      <li class="typography-subtitle border-r border-r-accent px-6 last:border-r-transparent">
-        <a href="/">System Status</a>
-      </li>
+  <footer class="w-full border-t">
+    <ul class="mx-auto mt-8 flex justify-center gap-8">
+      <li class="typography-subtitle2">Support</li>
+      <li class="typography-subtitle2">System Status</li>
     </ul>
   </footer>
-</div>
+</main>
